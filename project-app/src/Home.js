33c2747f9dc276eb72ac6ae4d139d165b1924481
Arnold_Kells <arnoldkells4@gmail.com>
2024-03-07 12:24:10 +0300

    added footer component in the home

--- project-app/src/Home.js
+++ project-app/src/Home.js
@@ -1,4 +1,5 @@
 import DeliciousFoods from "./DeliciuosFoods";
+import Footer from "./Footer";
 import PopularFoods from "./PopularFoods";
 import useFetch from "./useFetch";
 
@@ -12,6 +13,7 @@ const Home = () => {
       {error && <div className="error-message"><h4>{error}</h4></div>}
       {slideFoods && <PopularFoods foods={slideFoods} heading="Our Popular Foods" />}
       {foods && <DeliciousFoods foods={foods} heading="Delicious Foods" />}
+      <Footer />
     </div>
   );
 }
