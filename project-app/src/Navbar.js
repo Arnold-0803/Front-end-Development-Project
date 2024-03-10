7e9d8eae1ee94e390ab14f09acc13fbba66978e6
Arnold_Kells <arnoldkells4@gmail.com>
2024-03-10 15:59:08 +0300

    modified the nav-bar & the drop-down menu dynamic properties

--- project-app/src/Navbar.js
+++ project-app/src/Navbar.js
@@ -1,37 +1,51 @@
 import { useState } from "react";
 import { RiArrowDropUpLine, RiArrowDropDownLine } from "react-icons/ri";
 import { GiCampCookingPot  } from "react-icons/gi";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import SearchBar from "./Searchbar";
 
 const NavBar = () => {
+
+  const [toggle, setTaggle ] = useState(true);
+  const [upArrow, setArrowUp] = useState(true);
+  const [downArrow, setArrowDown] = useState(false);
+
+  const handleClick = () => {
+    setTaggle(prev => !prev);
+    setArrowUp(prev => !prev);
+    setArrowDown(prev => !prev);
+  }
+
   return (
     <div className="nav">
-      <Link to={'/'}><h1><span><GiCampCookingPot /></span>Cook</h1></Link>
+      <Link to={'/'}><h1><span><GiCampCookingPot /></span>jikoni</h1></Link>
       <SearchBar />
       <div className="links">
         <ul>
           <li>
-            <Link to={'/'}>Home</Link>
+            <NavLink className="home" to={'/'}>Home</NavLink>
           </li>
           <li className="exo-links">
-            <Link>Exotic cuisine</Link>
-            <RiArrowDropUpLine />
-            <RiArrowDropDownLine />
-            <ul>
-                <li>
-                  <Link to={'cuisine/Italian'}>Italian</Link>
-                </li>
-                <li>
-                  <Link to={'cuisine/American'}>American</Link>
-                </li>
-                <li>
-                  <Link to={'cuisine/Thai'}>Thai</Link>
-                </li>
-                <li>
-                  <Link to={'cuisine/Japanese'}>Japanese</Link>
-                </li>
-              </ul>
+            <Link className="drop-link"
+              onClick={handleClick}
+            >Exotic cuisine
+              <RiArrowDropUpLine id="up-arrow" className={upArrow ? "active" : ""} />
+              <RiArrowDropDownLine id="down-arrow" className={downArrow ? "active" : ""} />
+            </Link>
+            <ul className={toggle ? "inactive" : ""}>
+              <li className="active border">
+                <NavLink to={'cuisine/Italian'}>Italian</NavLink>
+              </li>
+              <li className="active border">
+                <NavLink to={'cuisine/American'}>American</NavLink>
+              </li>
+              <li className="active border">
+                <NavLink to={'cuisine/Thai'}>Thai</NavLink>
+              </li>
+              <li className="active">
+                <NavLink to={'cuisine/Japanese'}>Japanese</NavLink>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
